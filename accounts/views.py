95bef60986222a50947fe8d8df34dbca34a957f2
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-16 20:13:50 +0000

    Added csrf token to login form in accounts/templates/login.html, removed code and added login routing to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -11,18 +11,24 @@ def index(request):
 def logout(request):
     """Log the user out"""
     auth.logout(request)
-    return redirect(reverse('index'))
-
-
-def logout(request):
-    """Log the user out"""
-    auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
 
 
 def login(request):
     """Return a login page"""
-    login_form = UserLoginForm()
-    # passing in the form to the template, creating context dictionary.
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(
+                    None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
