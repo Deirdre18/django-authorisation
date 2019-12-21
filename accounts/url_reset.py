1b814463b7474ffe413d5542acfb1843a424809f
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-21 22:06:18 +0000

    added new file - accounts/url_reset.py, amended accounts/urls.py and django_auth/urls.py for reset password url and using email for resetting password.

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -1,16 +1,15 @@
 # all view from django which allows us to implement reset password functionality.
+
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
-urlpatterns = [  # base url
+# base url, done url, unique url (token generated for each user, create unique p/w - uid created. This is a unque url for reset password), password reset complete url.
+urlpatterns = [
     url('^$', password_reset, {'post_reset_redirect': reverse_lazy(
         'password_reset_done')}, name='password_reset'),
-    # done url
     url(r'^done/$', password_reset_done, name='password_reset_done'),
-    # token generated for eah user, create unique p/w - uid created. This is a unque url for reset password.
     url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
         {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    # password reset complete url
     url('^complete/$', password_reset_complete, name='password_reset_complete')
 ]
